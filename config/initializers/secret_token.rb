3c3b335f773910bd69fb6bbde63dc93ac550638c
James <james@James-PC.(none)>
2011-08-26 16:41:45 -0400

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Redwing::Application.config.secret_token = 'd45e44b6c4a687a608067af693d5b544cd0b0ac07675e51898fb8d8bb9975e6fce8e01e3be6cd74c8f2486b1fe602ce476d48484e326b1d6c9e2aa6f45a704b2'
