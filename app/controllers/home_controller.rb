9f316597f438a6b3195c5920f8f269beaeded3ed
James <james@James-PC.(none)>
2011-09-03 10:56:31 -0400

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,10 +19,25 @@ class HomeController < ActionController::Base
     @carrier = Carrier.order(:name => 'desc')
     @work_site = WorkSite.order(:name => 'desc') 
     @project_type = ProjectType.order(:name => 'desc')  
+    @scope_type = ScopeType.find_by_ProjectTypeID(@project_type.first.id)
     
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @job }
     end 
+  end
+  
+  def get_scope
+    is_render = false
+    if request.xhr? && params[:project_type]
+      @scope_type = ScopeType.find_by_ProjectTypeID(params[:project_type])
+      if @scope_type
+        is_render = true
+        render :partial => "home/scope_type"
+      end 
+    end
+    if ! is_render #render using the previous or restaurant zip
+      raise "false"
+    end
   end
 end
--- app/models/scope_type.rb
+++ app/models/scope_type.rb
@@ -1,2 +1,9 @@
 class ScopeType < ActiveRecord::Base
+  
+  class << self
+    def find_by_ProjectTypeID(project_type)
+      where(:ProjectTypeID => project_type).first
+    end
+  end    
+  
 end
