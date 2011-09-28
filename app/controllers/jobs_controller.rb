b5eba496d31a97fa325e30542427732be83c5e66
James <james@James-PC.(none)>
2011-09-28 00:52:19 -0400

    .

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,17 +5,25 @@ class JobsController < ActionController::Base
   in_place_edit_for :milestone_record, :date_completed
   protect_from_forgery
   layout "application"
+  
+  EDIT_TEXT = "click to edit"
   #test
    
   
   def set_milestone_record_date_completed
-    
-    render :text => "click to edit" and return if params[:value] == ""
+    @error = "*incorrect date format"
+    render :text => EDIT_TEXT and return if params[:value] == ""
     milestone_record = MilestoneRecord.find(params[:id])
-    if milestone_record.update_attribute(:date_completed, params[:value])   
+    old_value = milestone_record.date_completed
+    milestone_record.date_completed = params[:value]
+    if not params[:value] == EDIT_TEXT and milestone_record.valid?
+      milestone_record.save!
+      @error = nil
       render :text => params[:value]
+    elsif old_value
+      render :text => old_value  
     else
-      render :text => "Invalid Date"
+      render :text => EDIT_TEXT
     end  
   end
   
@@ -25,7 +33,7 @@ class JobsController < ActionController::Base
     if milestone_record.date_completed
     render :text => milestone_record.date_completed
     else
-      render :text => "click to edit"
+      render :text => EDIT_TEXT
     end
   end
   
@@ -36,11 +44,13 @@ class JobsController < ActionController::Base
   end
   
   def set_milestone_records(scope_type)
-    return unless scope_type
+    return unless scope_type and session[:job_id]
     recs = MilestoneTask.find_all_by_scope_type(scope_type)
-    @records = MilestoneRecord.find_all_by_jobs_id(@job.id)
+    job = Job.find(session[:job_id])
+    return unless job
+    @records = MilestoneRecord.find_all_by_jobs_id(job.id)
     if @records and @records.first
-      scope_test = @job.ScopeTypeID
+      scope_test = job.ScopeTypeID
       #need alert before this action!
       if scope_test == scope_type
         return
@@ -49,13 +59,13 @@ class JobsController < ActionController::Base
       end   
     end
     mid_arr = []
-    if @job and @job.id
+    if job and job.id
       recs.each do |r|
         
         ins = {
           :milestone_tasks_id => r.id,
-          :jobs_id => @job.id,
-          :date_completed => '-'
+          :jobs_id => job.id,
+          :date_completed => nil
         }
         mr = MilestoneRecord.new(ins)
         mr.save!
@@ -78,8 +88,9 @@ class JobsController < ActionController::Base
   
   def new
     session[:scope_type] = nil
+    session[:job_id] = nil
     @job = Job.new
-    @jobs.CustomerID = session[:CustomerID] if session[:CustomerID]
+    @job.CustomerID = session[:CustomerID] if session[:CustomerID]
     @customer = Customer.order(:name => 'desc')
     @carrier = Carrier.order(:name => 'desc')
     @work_site = WorkSite.order(:name => 'desc') 
@@ -136,6 +147,7 @@ class JobsController < ActionController::Base
   
   
   def show 
+    
     @job = Job.find(params[:id])
     @customer = Customer.order(:name => 'desc')
     @carrier = Carrier.order(:name => 'desc')
@@ -143,7 +155,9 @@ class JobsController < ActionController::Base
     @project_type = ProjectType.order(:name => 'desc')  
     @scope_type = ScopeType.find_all_by_ProjectTypeID(@job.ProjectTypeID)
     session[:scope_type] = @job.ScopeTypeID
+    session[:job_id] = @job.id
     set_milestone_records(@scope_type.first.id) if @scope_type.first
+    
   end
   
   def edit
